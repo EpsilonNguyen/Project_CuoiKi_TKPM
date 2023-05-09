c32c45fd2e75ecf0ece6f711ccaf99f7eba63be9
NTP <pudding2912@gmail.com>
2023-05-09 22:35:46 +0700

    update api admin

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -46,6 +46,40 @@ export const register = async (req, res, next) => {
     }
 }
 
+export const loginAdmin = async (req, res, next) => {
+    try {
+        const resultUser = await User.findOne({ email: req.body.email });
+        if (!resultUser) return next(createError(404, "User Không Tồn Tại!"));
+
+        const isLocking = resultUser.isLocked;
+        if (isLocking) return next(createError(404, "Tài khoản của bạn đã bị khóa vì một số vấn đề!"));
+
+        const isCorrectPassword = bcrypt.compareSync(req.body.password, resultUser.password);
+        if (!isCorrectPassword) return next(createError(404, "Mật khẩu không chính xác!"));
+
+        const userIsAdmin = resultUser.isAdmin;
+        if (!userIsAdmin) return next(createError(404, "Bạn không phải là Admin!"));
+
+        const token = jwt.sign({ id: resultUser._id, isAdmin: resultUser.isAdmin }, process.env.JWT);
+
+        const { password, isAdmin, isLocked, ...otherDetails } = resultUser._doc;
+        const birthDay = moment(resultUser.birthDay).format('DD-MM-YYYY');
+        const nowDate = moment(Date.now()).format('DD-MM-YYYY');
+        if (birthDay === nowDate) return res.cookie(
+            "access_token",
+            token,
+            {
+                httpOnly: true
+            }).status(200).send({ ...otherDetails });
+
+        res.cookie("access_token", token, {
+            httpOnly: true
+        }).status(200).send({ birthDay, ...otherDetails });
+    } catch (err) {
+        next(err);
+    }
+}
+
 export const login = async (req, res, next) => {
     try {
         const resultUser = await User.findOne({ email: req.body.email });
