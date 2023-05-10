e2a2d1298c9111c66ed4e94ea61abfc405cb1eea
fox <phurong1@gmail.com>
2023-05-11 00:40:40 +0700

    finish admin

--- admin/src/components/AddShoe.js
+++ admin/src/components/AddShoe.js
@@ -1,59 +1,184 @@
-import { TbShoe } from "react-icons/tb";
+import { useState } from 'react';
+import { TbShoe } from 'react-icons/tb';
+import axios from '../hooks/axios';
 
 const AddShoe = () => {
+    const [image, setImage] = useState(null);
+    const [data, setData] = useState({
+        name: '',
+        description: '',
+        brand: '',
+        quantity: '',
+        price: '',
+        sizes: [],
+    });
+    const [brand, setBrand] = useState(['Nike', 'Adidas', 'Vans', 'Balenciaga', 'Converse', 'Puma']);
+
+    const handleChange = (e) => {
+        setData((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+    };
+    console.log(data);
+    const handleUpload = (event) => {
+        const files = event.target.files; // Lấy danh sách tệp tin đã chọn
+        const arr = [];
+        for (let i = 0; i < files.length; i++) {
+            arr.push(files[i]);
+        }
+        setImage(arr);
+    };
+    const [selectedSizes, setSelectedSizes] = useState([]);
+    const handleSizeClick = (sizes) => {
+        if (selectedSizes.includes(sizes)) {
+            const newSelectedSizes = selectedSizes.filter((s) => s !== sizes);
+            setSelectedSizes(newSelectedSizes);
+            setData({ ...data, size: newSelectedSizes });
+        } else if (selectedSizes.length < 3) {
+            // nếu size chưa được chọn và số lượng size đã chọn < 3, ta sẽ thêm size vào mảng selectedSizes và cập nhật lại data.size
+            const newSelectedSizes = [...selectedSizes, sizes];
+            setSelectedSizes(newSelectedSizes);
+            setData({ ...data, sizes: newSelectedSizes });
+        }
+    };
+    console.log(image);
+    const handleSubmit = async () => {
+        const formData = new FormData();
+        image.forEach((i) => {
+            formData.append('images', i);
+        });
+        formData.append('name', data.name);
+        formData.append('description', data.description);
+        formData.append('brand', data.brand);
+        formData.append('quantity', data.quantity);
+        formData.append('price', data.price);
+        data.sizes.forEach((i) => {
+            formData.append('sizes', i);
+        });
+        try {
+            await axios.post('shoe/new', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+        } catch (err) {
+            console.log(err.message);
+        }
+    };
     return (
         <div className="flex flex-col gap-3 bg-white px-72">
             <div className="flex flex-col mt-4 gap-2">
                 <label>Name</label>
-                <input type="text" placeholder="Air Force 1" className="rounded-2xl border-2 border-gray-400 py-2 px-3" />
+                <input
+                    type="text"
+                    id="name"
+                    placeholder="Air Force 1"
+                    className="rounded-2xl border-2 border-gray-400 py-2 px-3"
+                    onChange={(e) => handleChange(e)}
+                    c
+                />
             </div>
             <div className="flex justify-center">
-                <input type="file"
+                <input
+                    type="file"
+                    id="images"
+                    multiple
                     accept="image/png, image/jpeg"
-                    className="h-[120px] rounded-2xl border-2 border-gray-400 pt-10 pl-16" />
+                    onChange={handleUpload}
+                    className="h-[120px] rounded-2xl border-2 border-gray-400 pt-10 pl-16"
+                />
             </div>
             <div className="flex flex-col gap-2">
                 <label>Description</label>
-                <textarea placeholder="Description for shoe"
-                    className="pl-3 py-2 rounded-2xl border-2 border-gray-400 h-[120px]" rows="4" cols="50">
-                </textarea>
+                <textarea
+                    placeholder="Description for shoe"
+                    className="pl-3 py-2 rounded-2xl border-2 border-gray-400 h-[120px]"
+                    rows="4"
+                    cols="50"
+                    id="description"
+                    onChange={(e) => handleChange(e)}
+                ></textarea>
             </div>
             <div className="flex justify-center gap-8">
                 <div className="flex flex-col">
                     <label>Brand</label>
-                    <select name="cars" id="cars"
-                        className="border-2 border-gray-400 rounded-2xl py-2 px-3 w-[150px]">
-                        <option value="volvo">Volvo</option>
-                        <option value="saab">Saab</option>
-                        <option value="opel">Opel</option>
-                        <option value="audi">Audi</option>
+                    <select
+                        onChange={(e) => handleChange(e)}
+                        id="brand"
+                        value={data.brand}
+                        className="border-2 border-gray-400 rounded-2xl py-2 px-3 w-[150px]"
+                    >
+                        {brand.map((item) => (
+                            <option id="brand" value={item}>
+                                {item}
+                            </option>
+                        ))}
                     </select>
                 </div>
                 <div className="flex flex-col">
                     <label>Quantity</label>
-                    <input type="text" placeholder="" className="rounded-2xl border-2 border-gray-400 py-2 px-3 w-[150px]" />
+                    <input
+                        type="text"
+                        placeholder=""
+                        id="quantity"
+                        onChange={(e) => handleChange(e)}
+                        className="rounded-2xl border-2 border-gray-400 py-2 px-3 w-[150px]"
+                    />
                 </div>
                 <div className="flex flex-col">
                     <label>Price</label>
-                    <input type="text" placeholder="" className="rounded-2xl border-2 border-gray-400 py-2 px-3 w-[150px]" />
+                    <input
+                        type="text"
+                        placeholder=""
+                        id="price"
+                        onChange={(e) => handleChange(e)}
+                        className="rounded-2xl border-2 border-gray-400 py-2 px-3 w-[150px]"
+                    />
                 </div>
             </div>
             <div className="flex flex-col">
                 <label>Sizes</label>
                 <div className="flex gap-5">
-                    <input type="button" value="32" className="rounded-2xl border-2 border-gray-400 py-2 px-3  w-[80px] text-center" />
-                    <input type="button" value="33" className="rounded-2xl border-2 border-gray-400 py-2 px-3  w-[80px] text-center" />
-                    <input type="button" value="34" className="rounded-2xl border-2 border-gray-400 py-2 px-3  w-[80px] text-center" />
-                    <input type="button" value="35" className="rounded-2xl border-2 border-gray-400 py-2 px-3  w-[80px] text-center" />
-                    <input type="button" value="36" className="rounded-2xl border-2 border-gray-400 py-2 px-3  w-[80px] text-center" />
+                    <input
+                        type="button"
+                        value="32"
+                        onClick={() => handleSizeClick(32)}
+                        className="rounded-2xl border-2 border-gray-400 py-2 px-3  w-[80px] text-center"
+                    />
+                    <input
+                        type="button"
+                        value="33"
+                        onClick={() => handleSizeClick(33)}
+                        className="rounded-2xl border-2 border-gray-400 py-2 px-3  w-[80px] text-center"
+                    />
+                    <input
+                        type="button"
+                        value="34"
+                        onClick={() => handleSizeClick(34)}
+                        className="rounded-2xl border-2 border-gray-400 py-2 px-3  w-[80px] text-center"
+                    />
+                    <input
+                        type="button"
+                        value="35"
+                        onClick={() => handleSizeClick(35)}
+                        className="rounded-2xl border-2 border-gray-400 py-2 px-3  w-[80px] text-center"
+                    />
+                    <input
+                        type="button"
+                        value="36"
+                        onClick={() => handleSizeClick(36)}
+                        className="rounded-2xl border-2 border-gray-400 py-2 px-3  w-[80px] text-center"
+                    />
                 </div>
             </div>
-            <button type="button" className="flex justify-center gap-3 rounded-2xl bg-teal-400 py-1 px-2 text-white w-[180px] mx-auto mb-3">
+            <button
+                onClick={handleSubmit}
+                type="button"
+                className="flex justify-center gap-3 rounded-2xl bg-teal-400 py-1 px-2 text-white w-[180px] mx-auto mb-3"
+            >
                 <TbShoe size={25} />
                 <span>Add Shoe</span>
             </button>
         </div>
-    )
-}
+    );
+};
 
 export default AddShoe;
--- backend/controllers/shoeController.js
+++ backend/controllers/shoeController.js
@@ -1,63 +1,63 @@
-import { createError } from "../utils/error.js";
-import Shoe from "../models/Shoe.js";
-import Review from "../models/Review.js";
-import cloudinary from "../utils/cloudinary.js";
-import Cart from "../models/Cart.js";
+import { createError } from '../utils/error.js';
+import Shoe from '../models/Shoe.js';
+import Review from '../models/Review.js';
+import cloudinary from '../utils/cloudinary.js';
+import Cart from '../models/Cart.js';
 export const infoHoyDealByBrand = async (req, res, next) => {
     try {
         const brand = req.params.brandName;
-        const hotDeal = await Shoe.find(
-            {
-                brand: brand,
-                sold: { $gt: 0 }
-            }
-        );
+        const hotDeal = await Shoe.find({
+            brand: brand,
+            sold: { $gt: 0 },
+        });
         res.status(200).send({
             success: true,
             total: hotDeal.length,
-            data: hotDeal
-        })
+            data: hotDeal,
+        });
     } catch (err) {
         next(err);
     }
-}
+};
 export const totalHotDealByBrand = async (req, res, next) => {
     try {
-        const listBrand = ["Adidas", "Nike", "Vans", "Balenciaga", "Converse", "Puma"];
-        const hotDeal = await Promise.all(listBrand.map((brand) =>
-            Shoe.find({
-                brand: brand,
-                sold: { $gt: 0 }
-            })
-        ));
+        const listBrand = ['Adidas', 'Nike', 'Vans', 'Balenciaga', 'Converse', 'Puma'];
+        const hotDeal = await Promise.all(
+            listBrand.map((brand) =>
+                Shoe.find({
+                    brand: brand,
+                    sold: { $gt: 0 },
+                }),
+            ),
+        );
         const total = hotDeal.map((deal) => deal.length);
         const result = listBrand.map((brand, index) => {
             return {
                 brand: brand,
-                total: total[index]
-            }
-        })
+                total: total[index],
+            };
+        });
 
         res.status(200).send({
             success: true,
-            data: result
-        })
+            data: result,
+        });
     } catch (err) {
         next(err);
     }
-}
+};
 
 export const shoeHotDeal = async (req, res, next) => {
     try {
         const listHotDeal = await Shoe.find().limit(10).sort({ sold: -1 });
         res.status(200).send({
             success: true,
-            data: listHotDeal
-        })
+            data: listHotDeal,
+        });
     } catch (err) {
         next(err);
     }
-}
+};
 
 export const searchShoe = async (req, res, next) => {
     try {
@@ -66,87 +66,88 @@ export const searchShoe = async (req, res, next) => {
         if (isNaN(query)) {
             resultShoe = await Shoe.find({
                 $or: [
-                    { name: { $regex: query, $options: "i" } },
-                    { description: { $regex: query, $options: "i" } },
+                    { name: { $regex: query, $options: 'i' } },
+                    { description: { $regex: query, $options: 'i' } },
                     { brand: query },
-                ]
-            })
+                ],
+            });
         } else {
             resultShoe = await Shoe.find({
                 $or: [
-                    { name: { $regex: query, $options: "i" } },
-                    { description: { $regex: query, $options: "i" } },
+                    { name: { $regex: query, $options: 'i' } },
+                    { description: { $regex: query, $options: 'i' } },
                     { brand: query },
                     { sizes: { $in: [query] } },
-                    { price: query }
-                ]
-            })
+                    { price: query },
+                ],
+            });
         }
 
-        if (!resultShoe.length) return res.status(200).send({
-            sucess: false,
-            message: "Không có Shoe thõa mãn!"
-        })
+        if (!resultShoe.length)
+            return res.status(200).send({
+                sucess: false,
+                message: 'Không có Shoe thõa mãn!',
+            });
 
         res.status(200).send({
             sucess: true,
             total: resultShoe.length,
-            data: resultShoe
-        })
+            data: resultShoe,
+        });
     } catch (err) {
         next(err);
     }
-}
+};
 
 export const getShoeByPrice = async (req, res, next) => {
     try {
         const listShoe = await Shoe.find({
             price: {
                 $gt: req.query.minPrice,
-                $lt: req.query.maxPrice
-            }
+                $lt: req.query.maxPrice,
+            },
         });
         res.status(200).send({
             sucess: true,
             total: listShoe.length,
-            data: listShoe
+            data: listShoe,
         });
     } catch (err) {
         next(err);
     }
-}
+};
 
 export const countShoeByBrand = async (req, res, next) => {
     try {
         const shoeBrand = req.params.brand;
         const listShoeBrand = await Shoe.find({
-            brand: shoeBrand
+            brand: shoeBrand,
         });
         res.status(200).send({
             sucess: true,
-            total: listShoeBrand.length
-        })
+            total: listShoeBrand.length,
+        });
     } catch (err) {
         next(err);
     }
-}
+};
 
 export const getShoeByBrand = async (req, res, next) => {
     try {
         const shoeBrand = req.params.brand;
         const listShoeBrand = await Shoe.find({
-            brand: shoeBrand
+            brand: shoeBrand,
         });
 
         if (listShoeBrand.length > 0) {
             res.status(200).send(listShoeBrand);
         } else {
-            res.status(200).send("Không có giày thuộc Brand " + shoeBrand);
+            res.status(200).send('Không có giày thuộc Brand ' + shoeBrand);
         }
     } catch (err) {
         next(err);
     }
-}
+};
 
 export const getShoeByID = async (req, res, next) => {
     try {
@@ -168,17 +169,17 @@ export const getShoeByID = async (req, res, next) => {
             {
                 $set: {
                     rating: averageRating,
-                    numRev: totalReview
-                }
+                    numRev: totalReview,
+                },
             },
-            { new: true }
+            { new: true },
         );
 
         res.status(200).send(saveShoe);
     } catch (err) {
         next(err);
     }
-}
+};
 
 export const getAllShoe = async (req, res, next) => {
     try {
@@ -187,7 +188,7 @@ export const getAllShoe = async (req, res, next) => {
     } catch (err) {
         next(err);
     }
-}
+};
 
 export const deleteImagesInShoe = async (req, res, next) => {
     try {
@@ -196,16 +197,13 @@ export const deleteImagesInShoe = async (req, res, next) => {
         const public_id = req.body.path.split('/').slice(-2).join('/').replace('.jpg', '');
         const result = await cloudinary.uploader.destroy(public_id);
 
-        if (result.result !== "ok") return next(createError(404, "Xóa Hình ảnh thất bại!"));
-        await Shoe.findByIdAndUpdate(
-            shoeID,
-            { $pull: { images: req.body.path } }
-        );
-        res.status(200).send("Xoá Hình ảnh thành công!");
+        if (result.result !== 'ok') return next(createError(404, 'Xóa Hình ảnh thất bại!'));
+        await Shoe.findByIdAndUpdate(shoeID, { $pull: { images: req.body.path } });
+        res.status(200).send('Xoá Hình ảnh thành công!');
     } catch (err) {
         next(err);
     }
-}
+};
 
 export const deleteShoe = async (req, res, next) => {
     try {
@@ -216,24 +214,21 @@ export const deleteShoe = async (req, res, next) => {
         const publics_id = shoeInfo.images.map((path) => path.split('/').slice(-2).join('/').replace('.jpg', ''));
         const result = await cloudinary.api.delete_resources(publics_id);
 
-        if (Object.values(result.deleted)[0] === "not_found") return next(createError(404, "Xóa Hình ảnh thất bại!"));
+        if (Object.values(result.deleted)[0] === 'not_found') return next(createError(404, 'Xóa Hình ảnh thất bại!'));
 
         await Promise.all(shoeInfo.reviews.map((review) => Review.findByIdAndDelete(review)));
 
         // CODE XÓA SHOE TRONG CART
-        await Cart.updateMany(
-            { shoeItem: { $elemMatch: { id: shoeID } } },
-            { $pull: { shoeItem: { id: shoeID } } }
-        )
+        await Cart.updateMany({ shoeItem: { $elemMatch: { id: shoeID } } }, { $pull: { shoeItem: { id: shoeID } } });
 
         const deleteShoe = await Shoe.findByIdAndDelete(shoeID);
-        if (!deleteShoe) return next(createError(404, "Giày cần xóa không tồn tại!"));
+        if (!deleteShoe) return next(createError(404, 'Giày cần xóa không tồn tại!'));
 
-        res.status(200).send("Giày đã được xóa thành công!");
+        res.status(200).send('Giày đã được xóa thành công!');
     } catch (err) {
         next(err);
     }
-}
+};
 
 export const updateShoe = async (req, res, next) => {
     try {
@@ -241,30 +236,32 @@ export const updateShoe = async (req, res, next) => {
 
         if (req.files) {
             req.body.images = req.files.map((file) => file.path);
-        };
+        }
 
-        const saveShoe = await Shoe.findByIdAndUpdate(
-            shoeID,
-            { $set: req.body },
-            { new: true });
+        const saveShoe = await Shoe.findByIdAndUpdate(shoeID, { $set: req.body }, { new: true });
 
         res.status(200).send(saveShoe);
     } catch (err) {
         next(err);
     }
-}
+};
 
 export const createShoe = async (req, res, next) => {
     try {
+        console.log('tet2', req?.file, req?.files);
+
         // Lấy link ảnh từ Cloudinary (đã upload trc đó)
         // console.log(req.files);
+        console.log('data', req.body);
+        // req.body.images = req.files.map((file) => file.path)
+        console.log(req.files);
         req.body.images = req.files.map((file) => file.path);
         const newShoe = new Shoe(req.body);
-
+        console.log(req.body);
         const saveShoe = await newShoe.save();
 
         res.status(200).json(saveShoe);
     } catch (err) {
         next(err);
     }
-}
+};
